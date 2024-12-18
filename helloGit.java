909521c37006cf79c6f14100b45ee7e6308c3bcf
Carlos <cromeroballester@gmail.com>
2024-12-19 00:53:35 +0100

    Cambios guardados

--- helloGit.java
+++ helloGit.java
@@ -1,5 +1,5 @@
 public class helloGit{
     public static void main(String[] args){
-        System.out.println("Nuevo Hola Git");
+        System.out.println("Nuevo Hola Git cambiado");
     }
 } //Hola buenas
