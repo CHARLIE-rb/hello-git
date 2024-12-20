230d0dc495fcdb4f396d49249f08b78634e8d523
Carlos <cromeroballester@gmail.com>
2024-12-20 18:35:07 +0100

    Segundo commit en github

--- helloGit.java
+++ helloGit.java
@@ -1,5 +1,6 @@
 public class helloGit{
     public static void main(String[] args){
         System.out.println("Hola github");
+        System.out.println("Hola github, esto es mi segundo commit");
     }
 } //Hola buenas
